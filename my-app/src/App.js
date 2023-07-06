25849dfd246eec7b4459284a3163535f39637130
Tarun <tarun.chn99@gmail.com>
2023-07-06 19:06:39 +0530

    commit my-app

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return(
+    
+          <div className = "App"> <h2>Hello</h2> 
+            
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate animi ab similique laborum facere, iste eveniet molestiae modi illo doloribus.</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum, perferendis!</p> 
+
+          </div>
+          
+  )
+
+}
+
+export default App;
