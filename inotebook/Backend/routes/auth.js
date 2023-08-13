45123e77dc277649aa20050091399b0b0902c3b4
Tarun <tarun.chn99@gmail.com>
2023-08-13 14:04:59 +0530

    cmt

--- inotebook/Backend/routes/auth.js
+++ inotebook/Backend/routes/auth.js
@@ -4,23 +4,37 @@ const User = require('../models/User')
 const {body, validationResult} = require('express-validator');
 
 router.get('/', [
+
     body('name','Enter atleast 3 characters').isLength({min:3}),
     body('email','Enter valid email').isEmail(),
     body('password','Enter atleast 5 characters').isLength({min:5})
-], (req,res) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
+], 
+    async (req,res) => {
+    
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
         return res.status(400).json({errors: errors.array()});
-    }
-    User.create({
-        name: req.body.name,
-        password: req.body.password,
-        email: req.body.email
-    }).then(user => res.json(user))
-    .catch(err => {
-        console.log(err)
-        res.json({error: 'Please enter uniquer value for email', message:err.message})
-    });
+        }
+
+        try{
+
+            let user = await User.findOne({email: req.body.email});
+            if(user){
+            return res.status(400).json({error: "Sorry! User with this mail exists"});
+        }
+
+        user = await User.create({
+            name: req.body.name,
+            password: req.body.password,
+            email: req.body.email
+        });        
+        
+        res.json(user);
+        }catch(error){
+            console.error(error.message);
+            res.status(500).send("Some error occured");
+        }
+
     });
 
 module.exports = router;
