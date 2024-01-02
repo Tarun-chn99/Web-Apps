4e78da6a019e49fc0ac0731090929dee109b59c2
tarun-chn99 <tarun.chn99@gmail.com>
2024-01-02 15:19:56 +0530

    cmt

--- React Apps/news-app/src/App.js
+++ React Apps/news-app/src/App.js
@@ -8,7 +8,7 @@ import LoadingBar from 'react-top-loading-bar'
 const App = () => {
 
   const pageSize = 5; 
-  const apiKey = process.env.REACT_APP_NEWS_API;
+  const apiKey = "69ac1739742646f081622e3248e083c3";
   const [progress, setProgress] = useState(0);
 
   const setprog = (progress) => {
@@ -23,7 +23,7 @@ const App = () => {
             progress={progress}
           />
           <Routes>
-            <Route exact path='/home'          element={<News setprog={setprog} key="home"          apiKey={apiKey} pageSize={pageSize} country="in" category="general" />} />
+            <Route exact path='/'              element={<News setprog={setprog} key="home"          apiKey={apiKey} pageSize={pageSize} country="in" category="general" />} />
             <Route exact path='/business'      element={<News setprog={setprog} key="business"      apiKey={apiKey} pageSize={pageSize} country="in" category="business" />} />
             <Route exact path='/entertainment' element={<News setprog={setprog} key="entertainment" apiKey={apiKey} pageSize={pageSize} country="in" category="entertainment" />} />
             <Route exact path='/health'        element={<News setprog={setprog} key="health"        apiKey={apiKey} pageSize={pageSize} country="in" category="health" />} />
--- React Apps/news-app/src/Components/Navbar.js
+++ React Apps/news-app/src/Components/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb lg-0">
-                        <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/home">Home</Link></li>
+                        <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/">Home</Link></li>
                         <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/business">Business</Link></li>
                         <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/entertainment">Entertainment</Link></li>
                         <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/health">Health</Link></li>
